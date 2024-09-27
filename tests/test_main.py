527eb88ce1a96b0f0737b220ee6aba35814e5f3f
vinicius.capitani <vinicius.gomes@capitanigroup.com>
2024-09-27 00:51:59 -0300

    Ajustando o PYTHONPATH diretamente no teste para corrigir importação

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
 from src.main import somar
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 def test_somar():
     assert somar(2, 3) == 5
