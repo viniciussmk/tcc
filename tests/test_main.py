7c8ca9113364d923569477f43c5e2a8eaf564199
vinicius.capitani <vinicius.gomes@capitanigroup.com>
2024-09-27 00:55:14 -0300

    Ajustando o caminho de importação no teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
-import os
 import sys
-
-from src.main import somar
+import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from src.main import somar
 
 def test_somar():
     assert somar(2, 3) == 5
